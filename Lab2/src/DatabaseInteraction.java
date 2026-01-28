b178e1ae2fc2dcb64940c340f563ca6e8ca22ee3
Nooh Alavi <noohalavi@gmail.com>
2026-01-28 08:46:04 -0500

    Added DatabaseInteraction file

---
+++ Lab2/src/DatabaseInteraction.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class DatabaseInteraction {
+    public String[] getData() {
+
+        List<Map<String, String>> dataList = new ArrayList<>();
+        Map<String, String> data1 = new HashMap<>();
+
+        return new String[5];
+    }
+
+    public boolean saveData(String[] data) {
+
+        return true;
+    }
+}
