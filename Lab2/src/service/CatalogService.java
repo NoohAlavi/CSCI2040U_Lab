55463f3e1a82f409f77d22bc9c6187dfd5b311c0
Nooh Alavi <noohalavi@gmail.com>
2026-01-28 09:26:12 -0500

    updated catalogservice.java and added write functionality

--- Lab2/src/service/CatalogService.java
+++ Lab2/src/service/CatalogService.java
@@ -1,11 +1,10 @@
 package service;
 
-import model.CatalogItem;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+import model.CatalogItem;
 
 public class CatalogService {
     private final List<CatalogItem> items = new ArrayList<>();
@@ -46,6 +45,28 @@ public class CatalogService {
         } catch (Exception e) {
             System.out.println("Failed to load CSV: " + e.getMessage());
         }
+    }
+
+    private static void writeToCsv(List<CatalogItem> items, String path) {
+        try (java.io.FileWriter fw = new java.io.FileWriter(path)) {
+            for (CatalogItem item : items) {
+                String line = item.getId() + "," + item.getName() + "," + item.getDescription();
+                System.out.println(line);
+                fw.write(line + "\n");
+            }
+        } catch (Exception e) {
+            System.out.println("Failed to write CSV: " + e.getMessage());
+        }
+    }
+
+    public static void main(String[] args) {
+        List<CatalogItem> items = new ArrayList<>();
+        
+        items.add(new CatalogItem(0, "test", "desc"));
+        items.add(new CatalogItem(1, "test2", "desc2"));
+        items.add(new CatalogItem(2, "test3", "desc3"));
+
+        writeToCsv(items, "Lab2/src/data/catalog.csv");
     }
 }
 
